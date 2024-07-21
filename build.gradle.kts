9ab19e499a637aaca64a2aa8a0ea0b5fc9a548e1
Vsevolod Cheverda <cheverda.seva@gmail.com>
2024-07-21 17:10:28 +0300

    Начало

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    kotlin("jvm") version "2.0.0"
+}
+
+group = "org.style_personal_bot"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven {
+        setUrl("https://jitpack.io")
+    }
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.1.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(18)
+}
