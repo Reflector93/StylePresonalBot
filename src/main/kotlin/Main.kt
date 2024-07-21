41b180bf4fc61e0778df18676f6f287fcef94566
Vsevolod Cheverda <cheverda.seva@gmail.com>
2024-07-21 17:10:59 +0300

    Начало

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,5 @@
 package org.style_personal_bot
 
-import com.github.kotlintelegrambot.entities.ChatId
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
